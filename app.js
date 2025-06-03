796b84296e37d5a19bba0d223683090c8bd0c274
Romain Royer <romain.royer78@gmail.com>
2025-06-03 10:54:03 +0200

    routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import connectDB from './config/db.js';
 import indexRouter from './routes/index.js';
 import pokemonRouter from './routes/pokemons.js';
 import trainerRouter from './routes/trainer.js'
+import livresRouter from './routes/livres.js';
 
 dotenv.config();
 connectDB();
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use('/', indexRouter);
 app.use('/pokemons', pokemonRouter);
 app.use('/trainers', trainerRouter)
+app.use('/livres', livresRouter);
 
 app.use(function(req, res, next) {
   next(createError(404));
